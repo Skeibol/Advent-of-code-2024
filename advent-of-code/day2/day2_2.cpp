3d5877cfe6fd5f0e5903ed269513601e9152b320
Skeibol <akezele35@gmail.com>
2024-12-02 21:23:07 +0100

    Runs well looks well Day 2

--- advent-of-code/day2/day2_2.cpp
+++ advent-of-code/day2/day2_2.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <vector>
 
-bool checkReportSafety(std::vector<int> &inputLine)
+bool checkReportSafetyWithDampener(std::vector<int> &inputLine)
 {
     int currentChar = 0;
     int prevChar = 0;
@@ -61,17 +61,55 @@ bool checkReportSafety(std::vector<int> &inputLine)
     }
     return false;
 }
+
+bool checkReportSafety(std::vector<int> &inputLine)
+{
+    int currentChar = 0;
+    int prevChar = 0;
+    int difference = 0;
+    int prevDifference = 0;
+    bool isValid = true;
+    for (int num : inputLine)
+    {
+
+        prevChar = currentChar;
+        currentChar = num;
+        if (prevChar == 0)
+        {
+            continue;
+        }
+
+        prevDifference = difference;
+        difference = currentChar - prevChar;
+
+        if (prevDifference >= 0 && difference > 0 && difference <= 3)
+        {
+            continue;
+        }
+        else if (prevDifference <= 0 && difference < 0 && difference >= -3)
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
     auto begin = std::chrono::high_resolution_clock::now();
-    std::vector<std::vector<int>> data;
 
     std::ifstream file("input.txt");
     std::vector<int> lineData;
     std::string line;
     std::stringstream lineStream;
+
     int cnt = 0;
-    int value;
+    int part1cnt = 0;
+    int parsedNumber;
+
     if (file.is_open())
     {
 
@@ -79,23 +117,29 @@ int main()
         {
             lineData = {};
 
-            value = 0;
+            parsedNumber = 0;
             lineStream.clear();
             lineStream.str(std::move(line));
 
-            while (lineStream >> value)
+            while (lineStream >> parsedNumber)
             {
-                lineData.push_back(value);
+                lineData.push_back(parsedNumber);
             }
-
             if (checkReportSafety(lineData))
+            {
+                part1cnt += 1;
+                cnt += 1;
+                continue;
+            }
+            if (checkReportSafetyWithDampener(lineData))
             {
                 cnt += 1;
             }
         }
         file.close();
     }
-    std::cout << cnt << "\n";
+    std::cout << "Part 1:" << part1cnt << "\n";
+    std::cout << "Part 2:" << cnt << "\n";
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1000000000.0f << "sec" << std::endl;
     return 0;
