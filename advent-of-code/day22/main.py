ef905826076262c5eea4acbf8eaf7a0a36e26850
Skeibol <akezele35@gmail.com>
2024-12-22 18:12:01 +0100

    Day22 binari

--- advent-of-code/day22/main.py
+++ advent-of-code/day22/main.py
@@ -10,36 +10,38 @@ def shift_left(arr, k):
     n = len(arr)
     k = min(k, n)  
     return arr[k:] + [0] * k
+def encode_number(n):
+    if not (-9 <= n <= 9):
+        raise ValueError("Number out of range (-9 to 9)")
+    return bytes([n & 0b11111])
+
 
 def calculateSecretNumber(initialNum,sequenceValues):
-    sequence = [0,0,0,0]
+    sequence = 0 
     prev = initialNum %10
     visitedSequences = {}
     for i in range(0,2000):
-      
-        
-   
         initialNum = (initialNum << 6 ^ initialNum  )% 16777216
         initialNum = (initialNum >> 5  ^ initialNum)% 16777216
         initialNum = (initialNum << 11 ^ initialNum  )% 16777216
        
         value = initialNum%10
-        change = value - prev
-        sequence[-1] = change
-        
-        if i>2 and value > 0:
-            seqTuple = tuple(sequence)
-            if seqTuple not in sequenceValues:
-                sequenceValues[seqTuple] = value
-                visitedSequences[seqTuple] = True
+       
+        encodedChange = value - prev & 0b11111  # Encode to 5 bits
+        sequence = (sequence << 5) | encodedChange  # Shift left and insert
+        sequence &= (1 << 20) - 1  # Truncate
+       
+      
+        if i>2 and value>0:
+            if sequence not in sequenceValues:
+                sequenceValues[sequence] = value
+                visitedSequences[sequence] = True
             else:
-                if seqTuple not in visitedSequences:
-                    sequenceValues[seqTuple] += value
-                    visitedSequences[seqTuple] = True
-                
+                if sequence not in visitedSequences:
+                    sequenceValues[sequence] += value
+                    visitedSequences[sequence] = True
                 
         prev = value
-        sequence = shift_left(sequence,1)
     return initialNum
 
 def sumList(ls):
